43723d75fb8a5daf97bee126797f63b40095b45c
Vasyl Diakonov <Vasyl.Diakonov@infopulse.com>
2019-03-28 17:42:20 +0200

    fix infinite loop

--- src/parser/fdepl.h
+++ src/parser/fdepl.h
@@ -26,7 +26,7 @@ using x3::_attr;
 using identifier_t = string;
 x3::rule<class identifier_id, identifier_t> const identifier { "identifier" };
 
-using propvalue_t = x3::variant<double, identifier_t>;
+using propvalue_t = x3::variant<bool, int, double, identifier_t>;
 x3::rule<class propvalue_id, propvalue_t> const propvalue { "propvalue" };
 using property_t = pair<identifier_t, propvalue_t>;
 x3::rule<class property_id, property_t> const property { "property" };
@@ -53,10 +53,10 @@ x3::rule<class dataset_id, dataset_t> const dataset { "dataset" };
 
 using fdepl_define_header_t = pair<fqn_t, fqn_t>;
 x3::rule<class fdepl_define_header_id, fdepl_define_header_t> const fdepl_define_header { "fdepl_define_header" };
-using fdepl_define_t = pair<fdepl_define_header_t, dataset_t>;
+using fdepl_define_t = pair<fdepl_define_header_t, boost::optional<dataset_t>>;
 x3::rule<class fdepl_define_id, fdepl_define_t> const fdepl_define { "fdepl_define" };
 
-using fdepl_full_t = pair<identifier_t, fdepl_define_t>;
+using fdepl_full_t = pair<identifier_t, boost::optional<fdepl_define_t>>;
 x3::rule<class fdepl_full_id, fdepl_full_t> const fdepl_full { "fdepl_full" };
 
 auto const whitespace
@@ -70,30 +70,20 @@ void introspect(T &ctx) {
    cout << "val type: " << getTypeName(_val(ctx)) << endl;
 };
 
-auto introspect_type = [](auto &ctx)
-{
-   introspect(ctx);
-   std::cout<<"FFFFFF"<<std::endl;
-   _val(ctx).push_back(_attr(ctx));
-};
-
 auto const identifier_def = x3::raw[lexeme[(x3::alpha | '_') >> *(x3::alnum | '_')]];
 auto const propvalue_def = bool_ | int_ | double_ | identifier;
 auto const property_def = identifier >> lit("=") >> propvalue;
-auto const propertyset_def = *(-lit(",") >> property);
+auto const propertyset_def = +(-lit(",") >> property);
 auto const fmember_def = identifier >> lit("{") >> propertyset >> lit("}");
 auto const fenum_def = lit("enumeration") >> identifier >> "{" >> *(fmember) >> "}";
 auto const fstruct_def = lit("struct") >> identifier >> "{" >> *(fmember) >> "}";
 auto const fqn_def = identifier % ".";
-auto const import_def = lit("import") >> lexeme['"' >> identifier[introspect_type] >> lit(".fidl") >> '"'];
+auto const import_def = lit("import") >> lexeme['"' >> identifier >> lit(".fidl") >> '"'];
 auto const data_def = propertyset | fenum | fstruct;
-auto const dataset_def = *(data[introspect_type]);
+auto const dataset_def = +(data);
 auto const fdepl_define_header_def = lit("define") >> fqn >> lit("for") >> lit("typeCollection") >> fqn;
-auto const fdepl_define_def =  fdepl_define_header >>
-                               lit("{") >>
-                               dataset >>
-                               lit("}");
-auto const fdepl_full_def = import[introspect_type] >> fdepl_define[introspect_type];
+auto const fdepl_define_def =  fdepl_define_header > lit("{") > -dataset > lit("}");
+auto const fdepl_full_def = import >> -fdepl_define;
 
 BOOST_SPIRIT_DEFINE(
    identifier,
